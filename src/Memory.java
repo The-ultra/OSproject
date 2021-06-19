a81da0ec3cd96bf1ffb6c1e86f028c5142c8d8cf
YousefAH <31350194+YousefAH@users.noreply.github.com>
2021-06-19 21:59:47 +0200

    memory.java

---
+++ src/Memory.java
@@ -0,0 +1,23 @@
+public class Memory {
+	MemoryBlock[] memory;
+	private int pointer;
+
+	public Memory() {
+		// TODO Auto-generated constructor stub
+		memory = new MemoryBlock[10];
+	}
+
+	public void addProcess(int id) {
+		try {
+			memory[pointer] = new MemoryBlock(id, pointer);
+			pointer++;
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("process limit reached");
+		}
+	}
+	
+	public MemoryBlock getblock(int indx) {
+		// TODO Auto-generated method stub
+		return memory[indx];
+	}
+}
